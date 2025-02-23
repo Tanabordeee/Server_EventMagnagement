6100fd15c9ee1f88c4b7df2bc2c77fe89e4bd0c7
Tanabordee Pateepbutsagon <102315593+Tanabordeee@users.noreply.github.com>
2025-02-23 23:08:03 +0700

    Add Favorite and Unfavorite and change service name

--- src/event/event.controller.ts
+++ src/event/event.controller.ts
@@ -3,78 +3,95 @@ import { EventService } from './event.service';
 import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import {EventUserService } from './EventUser.service';
+import { EventClubService } from './EventClub.service';
+import { EventAdminService } from './EventAdmin.service';
 @Controller('event')
 export class EventController {
-    constructor(private readonly eventService: EventService){}
+    constructor(private readonly eventService: EventService , private readonly EventUserService : EventUserService , private readonly EventClubService : EventClubService,
+        private readonly EventAdminService : EventAdminService
+    ){}
     @Get("/byuser")
     @UseGuards(JwtAuthGuard)
     getEvents(){
-        return this.eventService.FindAllEventByUser();
+        return this.EventUserService.FindAllEventByUser();
     }
 
     @Get("/namebyuser")
     @UseGuards(JwtAuthGuard)
     getEventName(@Query("eventName") eventName: string){
-        return this.eventService.FindEventNameByUser(eventName);
+        return this.EventUserService.FindEventNameByUser(eventName);
     }
 
     @Post("/create")
     @UseGuards(JwtAuthGuard)
     CreateEvent(@Request() req , @Body() CreateEventDto:CreateEventDto){
-        return this.eventService.CreateEventByClub(req.user.email , CreateEventDto);
+        return this.EventClubService.CreateEventByClub(req.user.email , CreateEventDto);
     }
 
     @Delete("/deletebyclub/:eventID")
     @UseGuards(JwtAuthGuard)
     DeleteEventByClub(@Request() req , @Param("eventID") eventID:string){
-        return this.eventService.DeleteEventByClub(req.user.email , eventID);
+        return this.EventClubService.DeleteEventByClub(req.user.email , eventID);
     }
 
     @Patch("/updatebyclub/:eventID")
     @UseGuards(JwtAuthGuard)
     UpdateEventByClub(@Request() req ,  @Param("eventID") eventID:string , UpdateEventDto:UpdateEventDto){
-        return this.eventService.UpdateEventByClub(req.user.email , eventID , UpdateEventDto);
+        return this.EventClubService.UpdateEventByClub(req.user.email , eventID , UpdateEventDto);
     }
 
     @Get("/getallbyclub")
     @UseGuards(JwtAuthGuard)
     GetAllEventByClub(@Request() req){
-        return this.eventService.FindAllEventByClub(req.user.email);
+        return this.EventClubService.FindAllEventByClub(req.user.email);
     }
 
     @Get("/getonebyclub/:eventID")
     @UseGuards(JwtAuthGuard)
     GetOneEventByClub(@Request() req , @Param("eventID") eventID:string){
-        return this.eventService.FindOneEventByClub(req.user.email , eventID);
+        return this.EventClubService.FindOneEventByClub(req.user.email , eventID);
     }
 
     @Put("/approvebyadmin/:eventID")
     @UseGuards(JwtAuthGuard)
     ApproveEventByAdmin(@Request() req , @Param('eventID') eventID: string, @Body("status") status:string){
-        return this.eventService.ApproveEventByAdmin(req.user.email , eventID , status);
+        return this.EventAdminService.ApproveEventByAdmin(req.user.email , eventID , status);
     }
 
     @Get("/getallbyadmin")
     @UseGuards(JwtAuthGuard)
     GetAllEventByAdmin(@Request() req){
-        return this.eventService.FindAllEventByAdmin(req.user.email);
+        return this.EventAdminService.FindAllEventByAdmin(req.user.email);
     }
 
     @Get("/getnamebyadmin")
     @UseGuards(JwtAuthGuard)
     GetNameEventByAdmin(@Request() req , @Body("eventName") eventName : string){
-        return this.eventService.FindEventNameByAdmin(req.user.email , eventName);
+        return this.EventAdminService.FindEventNameByAdmin(req.user.email , eventName);
     }
 
     @Delete("/deleteeventbyadmin/:eventID")
     @UseGuards(JwtAuthGuard)
     DeleteeventByAdmin(@Request() req , @Param("eventID") eventID:string){
-        return this.eventService.DeleteEventByAdmin(req.user.email , eventID);
+        return this.EventAdminService.DeleteEventByAdmin(req.user.email , eventID);
     }
 
     @Patch("/updateeventbyadmin/:eventID")
     @UseGuards(JwtAuthGuard)
     UpdateeventByAdmin(@Request() req , @Param("eventID") eventID:string ,  UpdateEventDto : UpdateEventDto){
-        return this.eventService.UpdateEventByAdmin(req.user.email ,eventID ,UpdateEventDto);
+        return this.EventAdminService.UpdateEventByAdmin(req.user.email ,eventID ,UpdateEventDto);
+    }
+
+    @Patch("/favorites/:eventID")
+    @UseGuards(JwtAuthGuard)
+    Favorites(@Request() req , @Param("eventID") eventID:string){
+        return this.eventService.FavoriteEvent(req.user.email , eventID);
+    }
+
+    @Patch("/unfavorite/:eventID")
+    @UseGuards(JwtAuthGuard)
+    Unfavorites(@Request() req , @Param("eventID") eventID:string){
+        return this.eventService.UnFavoriteEvent(req.user.email , eventID);
     }
 }
