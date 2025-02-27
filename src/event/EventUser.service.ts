a34ad9225507cffec1671c1880ab8ce1a8bc7458
Tanabordee Pateepbutsagon <102315593+Tanabordeee@users.noreply.github.com>
2025-02-27 14:06:36 +0700

    add to find status that only approve can see

--- src/event/EventUser.service.ts
+++ src/event/EventUser.service.ts
@@ -6,11 +6,11 @@ import { Repository } from 'typeorm';
 export class EventUserService{
     constructor(@InjectRepository(Event) private readonly EventRepository: Repository<Event>){}
         async FindAllEventByUser(): Promise<Event[]>{
-            return this.EventRepository.find({relations:["club"]});
+            return this.EventRepository.find({relations:["club"] , where:{status:"approve"}});
         }
     
         async FindEventNameByUser(eventName : string):Promise<Event[] | null>{
-            const events = await this.EventRepository.find({ where: { eventName } });
+            const events = await this.EventRepository.find({ where: { eventName , status:"approve" } });
             return events.length > 0 ? events : null;
         }
 }
