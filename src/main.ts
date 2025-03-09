a9e50e0ba8d4deaf246b2177ed74d33774257319
Tanabordee Pateepbutsagon <102315593+Tanabordeee@users.noreply.github.com>
2025-03-09 19:11:55 +0700

    upgrade limit payload

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import * as cookieParser from 'cookie-parser';
-
+import * as bodyParser from 'body-parser';
 // โหลดไฟล์ .env
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
   app.enableCors({
     origin: 'http://localhost:5173',
     credentials: true,
