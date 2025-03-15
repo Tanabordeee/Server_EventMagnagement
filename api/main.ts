f343c7e0bcba38a99f8a1f46cc21c9324c842b0e
Tanabordee Pateepbutsagon <102315593+Tanabordeee@users.noreply.github.com>
2025-03-15 16:44:25 +0700

    remove cookie parser

--- api/main.ts
+++ api/main.ts
@@ -1,8 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '../src/app.module'; 
 import * as dotenv from 'dotenv';
-import * as cookieParser from 'cookie-parser';
-import * as bodyParser from 'body-parser';
 
 dotenv.config();
 
@@ -10,9 +8,6 @@ let cachedServer: any;
 
 async function buildServer() {
   const app = await NestFactory.create(AppModule);
-  app.use(cookieParser());
-  app.use(bodyParser.json({ limit: '50mb' }));
-  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
   app.enableCors({
     origin: 'http://localhost:5173',
     credentials: true,
